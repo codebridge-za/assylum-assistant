a9bfaa831eb931358da8620791a35cd36d8bf8fc
William <78790926+williamflashforward@users.noreply.github.com>
2021-03-06 17:39:58 +0200

    ReadMe Edit, on how to run the solution

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 A Hackathon project for a CodeBridge Event
 
+## To test the Solution run :
+
+-Composer install
+-npm install
+-npm run dev
+-php artisan serve
+
+The Database is not yet setup
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,13 +27,13 @@
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
-              <div class="collapse navbar-collapse" id="navbarCollapse">
+              <div class="collapse navbar-collapse navbar-expand" id="navbarCollapse">
                 <ul class="navbar-nav me-auto mb-2 mb-md-0">
                   <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="#">Home</a>
                   </li>
                 </ul>
-                <select class="btn btn-sm btn-outline-secondary dropdown-toggle collapse">
+                <select class="btn btn-sm btn-outline-secondary dropdown-toggle">
                   <option selected>Nationality</option>
                   <option value="1">South African</option>
                   <option value="2">Congolese</option>
