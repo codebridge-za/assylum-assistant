a2901fd6c2006697ea6bfba4bd64695d0137e542
William <78790926+williamflashforward@users.noreply.github.com>
2021-03-06 15:17:31 +0200

    Homepage

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <title>Asylum Assistant</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/checkout/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="includes/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+  
+    <!-- Custom styles for this template -->
+    <link href="includes/css/form-validation.css" rel="stylesheet">
+  </head>
+  <body class="bg-light">
+    
+
+  @section('content')
+    <script src="includes/js/bootstrap.bundle.min.js"></script>
+    <script src="includes/js/form-validation.js"></script>
+</body>
+</html>
